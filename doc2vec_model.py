e5a193d90894cb199b73b2ba6987e54f28547ce7
sunyang1994702 <40493383+sunyang1994702@users.noreply.github.com>
2019-01-07 11:19:03 +0900

    Update doc2vec_model.py

--- doc2vec_model.py
+++ doc2vec_model.py
@@ -3,7 +3,7 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from gensim.test.utils import get_tmpfile
 
 
-review_file = "../file_package/RichmondHill_review.txt"
+review_file = "RichmondHill_review.txt"
 myfile = open(review_file, 'r')
 CORPUS = []
 for line_index, line in enumerate(myfile.readlines()):
@@ -12,7 +12,7 @@ for line_index, line in enumerate(myfile.readlines()):
 
 def doc_vec(CORPUS):
     TOKENIZED_CORPUS = normalize_corpus(CORPUS, True)
-    vector_size_list = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60,70,80,90,100,150,200]
+    vector_size_list = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
     for vs in vector_size_list:
         fname = get_tmpfile("doc2vec_model__" + str(vs))
         documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(TOKENIZED_CORPUS)]
@@ -21,16 +21,3 @@ def doc_vec(CORPUS):
 
 if __name__ == '__main__':
     doc_vec(CORPUS)
-
-
-
-
-"""
-vector_size_list = [5,10,15,20,25,30,35,40,45,50,55,60]
-for vs in vector_size_list:
-    fname = get_tmpfile("doc2vec_model_" + str(vs))
-    model = Doc2Vec.load(fname)
-
-    print(len(model[0]))
-    print(model[0])
-"""
